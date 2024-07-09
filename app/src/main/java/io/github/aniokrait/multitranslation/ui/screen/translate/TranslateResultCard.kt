49f6149d307ac7d8406080a5b4f7a848e18bcf9f
Aniokrait <itititcreclickhub@gmail.com>
2024-07-09 23:57:05 +0900

    Comply with google translate guideline.

--- app/src/main/java/io/github/aniokrait/multitranslation/ui/screen/translate/TranslateResultCard.kt
+++ app/src/main/java/io/github/aniokrait/multitranslation/ui/screen/translate/TranslateResultCard.kt
@@ -1,5 +1,7 @@
 package io.github.aniokrait.multitranslation.ui.screen.translate
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
@@ -40,66 +43,83 @@ fun TranslateResultCard(
     language: String,
     content: String,
 ) {
-    Card(
-        modifier = modifier
-            .fillMaxWidth(),
-    ) {
-        Column(
-            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+    Column {
+        Card(
+            modifier = modifier
+                .fillMaxWidth(),
         ) {
-            Row {
-                Text(
-                    modifier = Modifier,
-                    text = language,
-                    fontWeight = FontWeight.Bold,
-                )
-                Spacer(modifier = Modifier.weight(1f))
+            Column(
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+            ) {
+                Row {
+                    Text(
+                        modifier = Modifier,
+                        text = language,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
 
-                Box {
-                    val clipboardManager = LocalClipboardManager.current
-                    IconButton(
-                        modifier = Modifier
-                            .offset(x = 8.dp, y = (-8).dp),
-                        onClick = {
-                            clipboardManager.setText(AnnotatedString(content))
+                    Box {
+                        val clipboardManager = LocalClipboardManager.current
+                        IconButton(
+                            modifier = Modifier
+                                .offset(x = 8.dp, y = (-8).dp),
+                            onClick = {
+                                clipboardManager.setText(AnnotatedString(content))
+                            }
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.content_copy_24px),
+                                contentDescription = "copy"
+                            )
                         }
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.content_copy_24px),
-                            contentDescription = "copy"
-                        )
                     }
                 }
-            }
 
-            val scrollState = rememberScrollState()
+                val scrollState = rememberScrollState()
 //        Box {
-            val indentedText = buildAnnotatedString {
-                withStyle(style = SpanStyle()) {
-                    // indentation
-                    append("  ")
+                val indentedText = buildAnnotatedString {
+                    withStyle(style = SpanStyle()) {
+                        // indentation
+                        append("  ")
+                    }
+                    append(content)
                 }
-                append(content)
-            }
-            Text(
-                modifier = Modifier
-                    .height(textBlockHeight)
-                    .verticalScroll(scrollState),
-                text = indentedText,
-            )
+                Text(
+                    modifier = Modifier
+                        .height(textBlockHeight)
+                        .verticalScroll(scrollState),
+                    text = indentedText,
+                )
 
-            // TODO Fix to support Text label.
-            //  https://qiita.com/yasukotelin/items/fcf5b538fac922cb08a5
+                // TODO Fix to support Text label.
+                //  https://qiita.com/yasukotelin/items/fcf5b538fac922cb08a5
 //            ScrollBar(
 //                scrollState = scrollState,
 //                isAlwaysShowScrollBar = true,)
 //        }
-        }
 
+                if (content.isNotEmpty()) {
+                    Spacer(modifier = Modifier.weight(1f))
+                    Row {
+                        Spacer(modifier = Modifier.weight(1f))
+
+                        val uriHandler = LocalUriHandler.current
+                        Image(
+                            modifier = Modifier.clickable {
+                                uriHandler.openUri("http://translate.google.com")
+                            },
+                            painter = painterResource(id = R.drawable.powered_by_google),
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+        }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun TranslateResultCardPreview(
     @PreviewParameter(UserPreviewParameterProvider::class) content: Pair<String, String>,
