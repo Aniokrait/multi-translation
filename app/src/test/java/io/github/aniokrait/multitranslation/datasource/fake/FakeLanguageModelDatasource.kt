e1cf04374d7242dc39c73796e670876020fe140b
Aniokrait <itititcreclickhub@gmail.com>
2024-07-10 10:30:41 +0900

    Fix unit test

--- app/src/test/java/io/github/aniokrait/multitranslation/datasource/fake/FakeLanguageModelDatasource.kt
+++ app/src/test/java/io/github/aniokrait/multitranslation/datasource/fake/FakeLanguageModelDatasource.kt
@@ -1,8 +1,10 @@
 package io.github.aniokrait.multitranslation.datasource.fake
 
 import androidx.compose.runtime.mutableStateOf
+import com.google.mlkit.nl.translate.TranslateRemoteModel
 import io.github.aniokrait.multitranslation.repository.LanguageModelRepository
 import io.github.aniokrait.multitranslation.ui.stateholder.DownloadedState
+import io.mockk.spyk
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.util.Locale
@@ -19,6 +21,13 @@ class FakeLanguageModelDatasource : LanguageModelRepository {
     override fun getDownloadedInfo(): Flow<List<DownloadedState>> = flow {
         emit(list)
 
+    }
+
+    override suspend fun getDownloadedModels(): Set<TranslateRemoteModel> {
+        val japaneseRemoteModel: TranslateRemoteModel = spyk()
+        val englishRemoteModel: TranslateRemoteModel = spyk()
+
+        return setOf(japaneseRemoteModel, englishRemoteModel)
     }
 
     override suspend fun downloadModel(
