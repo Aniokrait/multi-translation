dc37c85253edb9db015bfa298b93ea1611582b29
Aniokrait <itititcreclickhub@gmail.com>
2024-07-23 12:25:25 +0900

    Update list on DeleteModelScreen when model has been deleted

--- app/src/main/java/io/github/aniokrait/multitranslation/viewmodel/DeleteModelViewModel.kt
+++ app/src/main/java/io/github/aniokrait/multitranslation/viewmodel/DeleteModelViewModel.kt
@@ -7,33 +7,42 @@ import androidx.lifecycle.viewModelScope
 import io.github.aniokrait.multitranslation.repository.LanguageModelRepository
 import io.github.aniokrait.multitranslation.ui.stateholder.EachLanguageState
 import io.github.aniokrait.multitranslation.ui.stateholder.uistate.DeleteModelUiState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import java.util.Locale
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class DeleteModelViewModel(
     private val repository: LanguageModelRepository,
 ) : ViewModel() {
-    private val checkState: StateFlow<Map<Locale, MutableState<Boolean>>> =
-        initCheckState()
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(),
-                initialValue = mapOf()
-            )
+    private val retryEvent: MutableStateFlow<RetryEvent> = MutableStateFlow(RetryEvent.Retrying)
 
+    private val checkStateFlow: StateFlow<Map<Locale, MutableState<Boolean>>> = retryEvent
+        .filter { it == RetryEvent.Retrying }
+        .flatMapLatest { initCheckState() }
+        .onEach {
+            retryEvent.value = RetryEvent.Idle
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = mapOf()
+        )
 
     private val isDeleting: MutableStateFlow<Boolean> = MutableStateFlow(false)
 
     val uiState: StateFlow<DeleteModelUiState> =
         combine(
-            checkState,
+            checkStateFlow,
             isDeleting,
         ) { checkState, isDeleting ->
             val languageState = checkState.map {
@@ -56,7 +65,8 @@ class DeleteModelViewModel(
     private fun initCheckState(): Flow<Map<Locale, MutableState<Boolean>>> = flow {
         val downloadedModels: Map<Locale, MutableState<Boolean>> =
             repository.getDownloadedModels().associate {
-                val checked = checkState.value[Locale.forLanguageTag(it.language)]?.value ?: false
+                val checked =
+                    checkStateFlow.value[Locale.forLanguageTag(it.language)]?.value ?: false
                 Locale.forLanguageTag(it.language) to mutableStateOf(checked)
             }
         emit(downloadedModels)
@@ -68,10 +78,12 @@ class DeleteModelViewModel(
      * @param targetLanguages which to delete from the user device.
      */
     fun onDeleteClicked(targetLanguages: List<Locale>) {
+        isDeleting.value = true
+
         viewModelScope.launch {
-            isDeleting.value = true
             repository.deleteModel(targetLanguages)
             isDeleting.value = false
+            retryEvent.value = RetryEvent.Retrying
         }
     }
 
@@ -81,6 +93,11 @@ class DeleteModelViewModel(
      * @param locale which to update checked state.
      */
     fun onCheckClicked(locale: Locale) {
-        checkState.value[locale]?.value = checkState.value[locale]?.value != true
+        checkStateFlow.value[locale]?.value = checkStateFlow.value[locale]?.value != true
     }
+}
+
+private sealed interface RetryEvent {
+    data object Idle : RetryEvent
+    data object Retrying : RetryEvent
 }
