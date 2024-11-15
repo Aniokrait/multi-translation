976e7017c3127a47dde305bafeb27ff5cb0c7235
Aniokrait <itititcreclickhub@gmail.com>
2024-11-15 20:30:19 +0900

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "io.github.aniokrait.multitranslation"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0"
+        versionCode = 6
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
