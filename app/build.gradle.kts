526c028eaa7a878ba5445304db991529f8ae91e7
Aniokrait <itititcreclickhub@gmail.com>
2024-11-15 11:24:59 +0900

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ plugins {
 
 android {
     namespace = "io.github.aniokrait.multitranslation"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.github.aniokrait.multitranslation"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 5
         versionName = "1.0"
 
